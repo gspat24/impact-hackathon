b27f89577d1815734af8a73851ffae52b7cadd47
GSPat1993 <gabriel.patalinjug@gmail.com>
2019-10-31 09:22:00 +0800

    initial commit maps

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/zone">Zone</router-link> |
-      <router-link to="/zone-type-management">Zone Type</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <google-map :center="{lat: 1.38, lng: 103.8}" :zoom="12" style="width: 100%; height: 500px">
+      <ground-overlay
+        source="./overlay.png"
+        :bounds="{
+            north: 1.502,
+            south: 1.185,
+            east: 104.0262,
+            west: 103.5998,
+        }"
+        :opacity="0.5"
+      ></ground-overlay>
+    </google-map>
   </div>
 </template>
+
 <script>
-export default {
-}
+export default {};
 </script>
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import * as VueGoogleMaps from 'vue2-google-maps'
 import './registerServiceWorker'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -9,6 +10,11 @@ import './assets/plenos-font-awesome.js'
 
 import Bootstrap from 'bootstrap-vue'
 Vue.use(Bootstrap)
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyBsv9x4puR4DMRukVWhRF-MYrzSGCeTPnA'
+  }
+})
 
 Vue.config.productionTip = false
 
