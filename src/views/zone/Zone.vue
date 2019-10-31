8ee94c25f7a9b4b934ca66a211e18223f60000aa
johnenrick <johnenrickplenos@gmail.com>
2019-10-31 12:23:47 +0800

    Hahay

--- src/views/zone/Zone.vue
+++ src/views/zone/Zone.vue
@@ -1,22 +1,22 @@
 <template>
   <div>
-    <zone-filter />
+    <!-- <zone-filter /> -->
     <zone-map ref="zoneMap" />
   </div>
 </template>
 <script>
 import ZoneMap from './ZoneMap'
-import ZoneFilter from './ZoneFilter'
+// import ZoneFilter from './ZoneFilter'
 import ZoneStore from '@/database/zone-store'
 export default {
   components: {
     ZoneMap,
-    ZoneFilter
+    // ZoneFilter
   },
   mounted(){
     setTimeout(() => {
       this.init()
-    }, 3000)
+    }, 300)
   },
   methods: {
     init(){
--- src/views/zone/ZoneMap.vue
+++ src/views/zone/ZoneMap.vue
@@ -1,9 +1,29 @@
 <template>
   <div>
+    {{zoneTypeLookUp[1]}}
+    <div class="text-left mx-5 mb-2">
+      <b-dropdown variant="outline-primary" >
+        <template v-slot:button-content variant="primary">
+          <fa icon="filter" /> Filter
+        </template>
+        <b-dropdown-form style="width: 360px">
+          <template v-for="zoneType in zoneTypeLookUp">
+            <b-form-checkbox
+              v-bind:key="zoneType['id']"
+              @change="changeFilter(zoneType['id'])"
+              name="checkbox-1"
+            >
+              <fa icon="circle" :color="zoneType['color']" />{{zoneType['description']}}
+            </b-form-checkbox>
+          </template>
+        </b-dropdown-form>
+      </b-dropdown>
+
+    </div>
     <l-map ref="myMap" :zoom="zoom" :center="center" style="width:100%; height:calc(100vh - 84px - 38px)">
       <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
       <template v-for="zone in zoneList">
-        <l-polygon v-bind:key="zone['id']" :lat-lngs="zone['coordinates']" tooltip="yawa" :fill-color="zoneTypeLookUp[zone['zone_type_id']]['color']" :fill-opacity="zone['keyAttributeValue'] / 100" :opacity="zone['keyAttributeValue'] / 110"  :color="zoneTypeLookUp[zone['zone_type_id']]['color']"></l-polygon>
+        <l-polygon v-if="zoneTypeLookUp[zone['zone_type_id']]['selected']" v-bind:key="zone['id']" :lat-lngs="zone['coordinates']" tooltip="yawa" :fill-color="zoneTypeLookUp[zone['zone_type_id']]['color']" :fill-opacity="zone['keyAttributeValue'] / 100" :opacity="zone['keyAttributeValue'] / 110"  :color="zoneTypeLookUp[zone['zone_type_id']]['color']"></l-polygon>
       </template>
     </l-map>
     <!-- <GmapMarker
@@ -25,6 +45,7 @@ export default {
   mounted () {
     let zoneTypes = ZoneTypeStore.state.zoneTypes
     for(let x = 0; x < zoneTypes.length; x++){
+      zoneTypes[x]['selected'] = true
       Vue.set(this.zoneTypeLookUp, zoneTypes[x]['id'], zoneTypes[x])
     }
     this.$nextTick(() => {
@@ -45,6 +66,9 @@ export default {
   methods: {
     _setZones(zoneList){
       this.zoneList = zoneList
+    },
+    changeFilter(zoneTypeId){
+      Vue.set(this.zoneTypeLookUp[zoneTypeId], 'selected', !this.zoneTypeLookUp[zoneTypeId]['selected'])
     }
   },
   computed: {
